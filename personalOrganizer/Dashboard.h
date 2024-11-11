2f19b7bc002f56f3d9ac29f27b8cf30789ab1410
kavindamalwewa <k1999nirmal@gmail.com>
2024-11-11 12:30:23 +0530

    Create files

---
+++ personalOrganizer/Dashboard.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace personalOrganizer {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Dashboard
+	/// </summary>
+	public ref class Dashboard : public System::Windows::Forms::Form
+	{
+	public:
+		Dashboard(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Dashboard()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// Dashboard
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(885, 488);
+			this->Name = L"Dashboard";
+			this->Text = L"Dashboard";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
