542e8dbf4d89da470ac270128e0f595b0708532d
kavindamalwewa <k1999nirmal@gmail.com>
2024-11-12 00:09:23 +0530

    Add exit button

--- personalOrganizer/Income.h
+++ personalOrganizer/Income.h
@@ -47,6 +47,12 @@ namespace personalOrganizer {
 	private: System::Windows::Forms::Label^ label3;
 	private: System::Windows::Forms::Label^ label4;
 	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::Button^ btnaddincome;
+	private: System::Windows::Forms::Button^ btnexit;
+
+
 
 
 	protected:
@@ -72,13 +78,17 @@ namespace personalOrganizer {
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->btnaddincome = (gcnew System::Windows::Forms::Button());
+			this->btnexit = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// dateTimePicker1
 			// 
 			this->dateTimePicker1->CalendarFont = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Bold,
 				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
-			this->dateTimePicker1->Location = System::Drawing::Point(98, 46);
+			this->dateTimePicker1->Location = System::Drawing::Point(99, 96);
 			this->dateTimePicker1->Name = L"dateTimePicker1";
 			this->dateTimePicker1->Size = System::Drawing::Size(255, 22);
 			this->dateTimePicker1->TabIndex = 0;
@@ -91,24 +101,24 @@ namespace personalOrganizer {
 				L"Job", L"Allowance from parents", L"Scholarships",
 					L"Student Aid"
 			});
-			this->comboBox1->Location = System::Drawing::Point(591, 46);
+			this->comboBox1->Location = System::Drawing::Point(164, 42);
 			this->comboBox1->Name = L"comboBox1";
 			this->comboBox1->Size = System::Drawing::Size(190, 24);
 			this->comboBox1->TabIndex = 2;
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(151, 98);
+			this->textBox1->Location = System::Drawing::Point(136, 139);
 			this->textBox1->Multiline = true;
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(365, 82);
+			this->textBox1->Size = System::Drawing::Size(548, 91);
 			this->textBox1->TabIndex = 4;
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(646, 98);
+			this->textBox2->Location = System::Drawing::Point(534, 94);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(135, 22);
+			this->textBox2->Size = System::Drawing::Size(229, 22);
 			this->textBox2->TabIndex = 5;
 			// 
 			// label2
@@ -116,7 +126,7 @@ namespace personalOrganizer {
 			this->label2->AutoSize = true;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(414, 50);
+			this->label2->Location = System::Drawing::Point(12, 46);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(146, 20);
 			this->label2->TabIndex = 6;
@@ -127,7 +137,7 @@ namespace personalOrganizer {
 			this->label3->AutoSize = true;
 			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label3->Location = System::Drawing::Point(12, 113);
+			this->label3->Location = System::Drawing::Point(12, 148);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(118, 20);
 			this->label3->TabIndex = 6;
@@ -139,7 +149,7 @@ namespace personalOrganizer {
 			this->label4->AutoSize = true;
 			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label4->Location = System::Drawing::Point(540, 100);
+			this->label4->Location = System::Drawing::Point(388, 96);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(84, 20);
 			this->label4->TabIndex = 6;
@@ -150,22 +160,67 @@ namespace personalOrganizer {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(12, 50);
+			this->label1->Location = System::Drawing::Point(12, 98);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(61, 20);
 			this->label1->TabIndex = 6;
 			this->label1->Text = L"Date :";
 			this->label1->Click += gcnew System::EventHandler(this, &Income::label3_Click);
 			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label5->Location = System::Drawing::Point(388, 46);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(129, 20);
+			this->label5->TabIndex = 6;
+			this->label5->Text = L"Income Name:";
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(534, 42);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(229, 22);
+			this->textBox3->TabIndex = 5;
+			// 
+			// btnaddincome
+			// 
+			this->btnaddincome->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnaddincome->Location = System::Drawing::Point(215, 268);
+			this->btnaddincome->Name = L"btnaddincome";
+			this->btnaddincome->Size = System::Drawing::Size(139, 36);
+			this->btnaddincome->TabIndex = 7;
+			this->btnaddincome->Text = L"Add Income";
+			this->btnaddincome->UseVisualStyleBackColor = true;
+			// 
+			// btnexit
+			// 
+			this->btnexit->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnexit->Location = System::Drawing::Point(534, 268);
+			this->btnexit->Name = L"btnexit";
+			this->btnexit->Size = System::Drawing::Size(126, 36);
+			this->btnexit->TabIndex = 7;
+			this->btnexit->Text = L"Exit";
+			this->btnexit->UseVisualStyleBackColor = true;
+			this->btnexit->Click += gcnew System::EventHandler(this, &Income::button2_Click);
+			// 
 			// Income
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(825, 446);
+			this->ClientSize = System::Drawing::Size(825, 355);
+			this->Controls->Add(this->btnexit);
+			this->Controls->Add(this->btnaddincome);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label5);
 			this->Controls->Add(this->label2);
+			this->Controls->Add(this->textBox3);
 			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->comboBox1);
@@ -185,6 +240,9 @@ namespace personalOrganizer {
 private: System::Void label3_Click(System::Object^ sender, System::EventArgs^ e) {
 }
 private: System::Void dateTimePicker1_ValueChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	this->Close();
 }
 };
 }
