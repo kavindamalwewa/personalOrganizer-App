994727e4d55b9557f735ce7ec093ba4570d57fe5
Kavinda Malwewa <k1999nirmal@gmail.com>
2025-03-11 11:48:17 +0530

    Update register form background

--- personalOrganizer/RegisterForm.h
+++ personalOrganizer/RegisterForm.h
@@ -58,6 +58,8 @@ namespace personalOrganizer {
 	private: System::Windows::Forms::Button^ btncancel;
 	private: System::Windows::Forms::Label^ label8;
 	private: System::Windows::Forms::LinkLabel^ lllogin;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::Label^ label9;
 
 
 
@@ -81,6 +83,7 @@ namespace personalOrganizer {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(RegisterForm::typeid));
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->tbname = (gcnew System::Windows::Forms::TextBox());
@@ -98,25 +101,28 @@ namespace personalOrganizer {
 			this->btncancel = (gcnew System::Windows::Forms::Button());
 			this->label8 = (gcnew System::Windows::Forms::Label());
 			this->lllogin = (gcnew System::Windows::Forms::LinkLabel());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->label9 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// label1
 			// 
-			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 28.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(-1, 8);
+			this->label1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 25.8F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(666, 39);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(600, 59);
 			this->label1->TabIndex = 0;
-			this->label1->Text = L"Register";
+			this->label1->Text = L"Create Your Account";
 			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
 			this->label1->Click += gcnew System::EventHandler(this, &RegisterForm::label1_Click);
 			// 
 			// label2
 			// 
-			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(13, 92);
+			this->label2->Font = (gcnew System::Drawing::Font(L"Times New Roman", 13.8F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(688, 156);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(142, 38);
 			this->label2->TabIndex = 1;
@@ -125,7 +131,7 @@ namespace personalOrganizer {
 			// 
 			// tbname
 			// 
-			this->tbname->Location = System::Drawing::Point(218, 89);
+			this->tbname->Location = System::Drawing::Point(882, 150);
 			this->tbname->Name = L"tbname";
 			this->tbname->Size = System::Drawing::Size(362, 34);
 			this->tbname->TabIndex = 2;
@@ -133,9 +139,9 @@ namespace personalOrganizer {
 			// 
 			// label3
 			// 
-			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label3->Location = System::Drawing::Point(13, 148);
+			this->label3->Font = (gcnew System::Drawing::Font(L"Times New Roman", 13.8F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(688, 203);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(142, 38);
 			this->label3->TabIndex = 1;
@@ -144,16 +150,16 @@ namespace personalOrganizer {
 			// 
 			// tbemail
 			// 
-			this->tbemail->Location = System::Drawing::Point(218, 145);
+			this->tbemail->Location = System::Drawing::Point(882, 197);
 			this->tbemail->Name = L"tbemail";
 			this->tbemail->Size = System::Drawing::Size(362, 34);
 			this->tbemail->TabIndex = 2;
 			// 
 			// label4
 			// 
-			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label4->Location = System::Drawing::Point(13, 206);
+			this->label4->Font = (gcnew System::Drawing::Font(L"Times New Roman", 13.8F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->label4->Location = System::Drawing::Point(688, 247);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(186, 38);
 			this->label4->TabIndex = 1;
@@ -162,16 +168,16 @@ namespace personalOrganizer {
 			// 
 			// tbphone
 			// 
-			this->tbphone->Location = System::Drawing::Point(218, 203);
+			this->tbphone->Location = System::Drawing::Point(880, 241);
 			this->tbphone->Name = L"tbphone";
 			this->tbphone->Size = System::Drawing::Size(362, 34);
 			this->tbphone->TabIndex = 2;
 			// 
 			// label5
 			// 
-			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label5->Location = System::Drawing::Point(13, 259);
+			this->label5->Font = (gcnew System::Drawing::Font(L"Times New Roman", 13.8F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->label5->Location = System::Drawing::Point(688, 288);
 			this->label5->Name = L"label5";
 			this->label5->Size = System::Drawing::Size(142, 38);
 			this->label5->TabIndex = 1;
@@ -180,16 +186,16 @@ namespace personalOrganizer {
 			// 
 			// tbaddress
 			// 
-			this->tbaddress->Location = System::Drawing::Point(218, 256);
+			this->tbaddress->Location = System::Drawing::Point(880, 282);
 			this->tbaddress->Name = L"tbaddress";
 			this->tbaddress->Size = System::Drawing::Size(362, 34);
 			this->tbaddress->TabIndex = 2;
 			// 
 			// label6
 			// 
-			this->label6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label6->Location = System::Drawing::Point(13, 311);
+			this->label6->Font = (gcnew System::Drawing::Font(L"Times New Roman", 13.8F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->label6->Location = System::Drawing::Point(685, 336);
 			this->label6->Name = L"label6";
 			this->label6->Size = System::Drawing::Size(142, 38);
 			this->label6->TabIndex = 1;
@@ -198,7 +204,7 @@ namespace personalOrganizer {
 			// 
 			// tbpassword
 			// 
-			this->tbpassword->Location = System::Drawing::Point(218, 308);
+			this->tbpassword->Location = System::Drawing::Point(880, 330);
 			this->tbpassword->Name = L"tbpassword";
 			this->tbpassword->PasswordChar = '*';
 			this->tbpassword->Size = System::Drawing::Size(362, 34);
@@ -206,9 +212,9 @@ namespace personalOrganizer {
 			// 
 			// label7
 			// 
-			this->label7->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label7->Location = System::Drawing::Point(13, 364);
+			this->label7->Font = (gcnew System::Drawing::Font(L"Times New Roman", 13.8F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->label7->Location = System::Drawing::Point(685, 386);
 			this->label7->Name = L"label7";
 			this->label7->Size = System::Drawing::Size(213, 38);
 			this->label7->TabIndex = 1;
@@ -217,7 +223,7 @@ namespace personalOrganizer {
 			// 
 			// tbconfirmpassword
 			// 
-			this->tbconfirmpassword->Location = System::Drawing::Point(218, 361);
+			this->tbconfirmpassword->Location = System::Drawing::Point(880, 380);
 			this->tbconfirmpassword->Name = L"tbconfirmpassword";
 			this->tbconfirmpassword->PasswordChar = '*';
 			this->tbconfirmpassword->Size = System::Drawing::Size(362, 34);
@@ -225,29 +231,40 @@ namespace personalOrganizer {
 			// 
 			// btnok
 			// 
-			this->btnok->Location = System::Drawing::Point(114, 422);
+			this->btnok->BackColor = System::Drawing::Color::Blue;
+			this->btnok->Font = (gcnew System::Drawing::Font(L"Times New Roman", 16.2F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->btnok->ForeColor = System::Drawing::Color::Snow;
+			this->btnok->Location = System::Drawing::Point(770, 465);
 			this->btnok->Name = L"btnok";
 			this->btnok->Size = System::Drawing::Size(159, 42);
 			this->btnok->TabIndex = 3;
-			this->btnok->Text = L"OK";
-			this->btnok->UseVisualStyleBackColor = true;
+			this->btnok->Text = L"Register";
+			this->btnok->UseVisualStyleBackColor = false;
 			this->btnok->Click += gcnew System::EventHandler(this, &RegisterForm::btnok_Click);
 			// 
 			// btncancel
 			// 
-			this->btncancel->Location = System::Drawing::Point(342, 422);
+			this->btncancel->BackColor = System::Drawing::Color::Blue;
+			this->btncancel->Font = (gcnew System::Drawing::Font(L"Times New Roman", 16.2F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->btncancel->ForeColor = System::Drawing::Color::Snow;
+			this->btncancel->Location = System::Drawing::Point(1038, 465);
+			this->btncancel->Margin = System::Windows::Forms::Padding(0);
 			this->btncancel->Name = L"btncancel";
 			this->btncancel->Size = System::Drawing::Size(159, 42);
 			this->btncancel->TabIndex = 3;
 			this->btncancel->Text = L"Cancel";
-			this->btncancel->UseVisualStyleBackColor = true;
+			this->btncancel->UseVisualStyleBackColor = false;
 			this->btncancel->Click += gcnew System::EventHandler(this, &RegisterForm::btncancel_Click);
 			// 
 			// label8
 			// 
-			this->label8->Location = System::Drawing::Point(96, 485);
+			this->label8->Font = (gcnew System::Drawing::Font(L"High Tower Text", 13.8F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->label8->Location = System::Drawing::Point(789, 532);
 			this->label8->Name = L"label8";
-			this->label8->Size = System::Drawing::Size(321, 32);
+			this->label8->Size = System::Drawing::Size(259, 32);
 			this->label8->TabIndex = 4;
 			this->label8->Text = L"Already have an account\? \r\n";
 			this->label8->Click += gcnew System::EventHandler(this, &RegisterForm::label8_Click);
@@ -255,20 +272,42 @@ namespace personalOrganizer {
 			// lllogin
 			// 
 			this->lllogin->AutoSize = true;
-			this->lllogin->Location = System::Drawing::Point(375, 485);
+			this->lllogin->Font = (gcnew System::Drawing::Font(L"Times New Roman", 13.8F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->lllogin->Location = System::Drawing::Point(1054, 532);
 			this->lllogin->Name = L"lllogin";
-			this->lllogin->Size = System::Drawing::Size(73, 29);
+			this->lllogin->Size = System::Drawing::Size(67, 26);
 			this->lllogin->TabIndex = 5;
 			this->lllogin->TabStop = true;
 			this->lllogin->Text = L"Login";
 			this->lllogin->LinkClicked += gcnew System::Windows::Forms::LinkLabelLinkClickedEventHandler(this, &RegisterForm::linkLabel1_LinkClicked);
 			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(-4, 1);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(660, 585);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 6;
+			this->pictureBox1->TabStop = false;
+			// 
+			// label9
+			// 
+			this->label9->Location = System::Drawing::Point(697, 108);
+			this->label9->Name = L"label9";
+			this->label9->Size = System::Drawing::Size(168, 39);
+			this->label9->TabIndex = 7;
+			// 
 			// RegisterForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(14, 29);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->BackColor = System::Drawing::Color::Cyan;
-			this->ClientSize = System::Drawing::Size(603, 538);
+			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(128)), static_cast<System::Int32>(static_cast<System::Byte>(125)),
+				static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->ClientSize = System::Drawing::Size(1278, 583);
+			this->Controls->Add(this->label9);
+			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->lllogin);
 			this->Controls->Add(this->label8);
 			this->Controls->Add(this->btncancel);
@@ -293,6 +332,7 @@ namespace personalOrganizer {
 			this->Name = L"RegisterForm";
 			this->Text = L"Register Form";
 			this->Load += gcnew System::EventHandler(this, &RegisterForm::RegisterForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
